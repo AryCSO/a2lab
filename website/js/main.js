28aaf54ac72a2820d1bf186b3bc5f2d2c9c1e7bc
AryCSO <arycarvalho1969@gmail.com>
2026-02-06 16:09:20 -0300

    pc gamer

--- website/js/main.js
+++ website/js/main.js
@@ -73,7 +73,7 @@ const SERVICOS = [
   },
   {
     id: 'montagem',
-    titulo: 'Montagem Gamer',
+    titulo: 'Montagem PC Gamer',
     descricao: 'Builds personalizadas de alta performance para jogos.',
     icone: `<svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/></svg>`,
     itens: [
